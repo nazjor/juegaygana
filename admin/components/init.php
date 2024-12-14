19fa083290ae6471dfade1a805dd47ed284f061c
nazjor <nazjorsolution@gmail.com>
2024-12-14 12:21:06 +0100

    fix: database

--- admin/components/init.php
+++ admin/components/init.php
@@ -1,5 +1,5 @@
 <?php 
 
 require_once __DIR__ . '../../../conf/config.php';
-require_once DIRPAGE_ADMIN . 'config/Database.php';
+require_once DIRPAGE_ADMIN . 'config/database.php';
 require_once DIRPAGE_ADMIN . 'repositories/BaseRepository.php';
