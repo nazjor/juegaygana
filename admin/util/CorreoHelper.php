80be2c002dd189877ea5a6b7e96008666c892120
nazjor <nazjorsolution@gmail.com>
2024-12-15 12:46:05 +0100

    feat: color boton

--- admin/util/CorreoHelper.php
+++ admin/util/CorreoHelper.php
@@ -36,7 +36,7 @@ class CorreoHelper {
             <html>
             <body style='font-family: Arial, sans-serif; background-color: #f4f4f4; padding: 20px;'>
                 <div style='width: 600px; margin: 0 auto; background-color: #fff; border-radius: 10px; box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1);'>
-                    <div style='background-color: #333; color: #fff; padding: 20px; text-align: center; border-radius: 10px 10px 0 0;'>
+                    <div style='background-color:rgb(0, 23, 47); color: #fff; padding: 20px; text-align: center; border-radius: 10px 10px 0 0;'>
                         <img src='$logoUrl' alt='Logo' style='width: 100px;'>
                         <h2 style='margin-top: 10px;'>¡Compra aprobada!</h2>
                     </div>
@@ -45,7 +45,7 @@ class CorreoHelper {
                         <p>Tu compra <strong>[$codigoCompra]</strong> ha sido aprobada.</p>
                         <p>Puedes consultar tus boletos en el siguiente enlace. Asegúrate de no compartirlo con nadie:</p>
                         <p style='text-align: center; margin: 20px 0;'>
-                            <a href='$enlaceRecibo' style='display: inline-block; background-color: #007bff; color: #fff; text-decoration: none; padding: 10px 20px; border-radius: 5px;'>Ver boletos</a>
+                            <a href='$enlaceRecibo' style='display: inline-block; background-color: rgb(14, 122, 0);; color: #fff; text-decoration: none; padding: 10px 20px; border-radius: 5px;'>Ver boletos</a>
                         </p>
                         <p style='font-size: 0.9em; color: #888;'>Si tienes alguna pregunta, no dudes en contactarnos.</p>
                     </div>
